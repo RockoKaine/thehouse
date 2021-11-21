dd15dd1b8cc6cb702a05c53f794e2a03368df2fb
Rocko <rockokaine@gmail.com>
2021-11-21 08:51:45 -0800

    starting game logic

---
+++ incs/js/story.js
@@ -0,0 +1,5 @@
+const story = {
+    opening: `
+    You are strolling through the woods near your house one day. It is getting hot and are beginning to feel that you require shade. ${responses.bonus === 'luck' ? 'Almost as if by LUCK you find the perfect shaded spot to rest' : 'You look around and see what you PERCEIVE to be the perfect spot to get some shade'}, it is under a great willow tree overlooking a valley with a large pond near it's east end. You must have walked farther than you realized, you have walked these woods before, but you don't recognize the area. But you're not lost. You sit with your back against the tree gazing over the valley eyes drawn to the pond. Your heart skips a beat, and for a moment your stomach drops. It must be the heat, you reach in your bag and sip water from your canteen(Reveal in inventory). You close your eyes for what you only plan to be a moment, but as you start opening your eyes you realize you fell asleep. The sun is setting over the pond, lighting up a mansion near it's edge.
+    `
+}
